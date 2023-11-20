c2c52207c775e442d83d9a123101968c57fc67f8
Yechan Choi <75158889+Mellow1213@users.noreply.github.com>
2023-11-20 16:44:02 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N; // N은 사용하지 않음
+
+    // 첫 번째 줄에 "1" 출력
+    cout << "1\n";
+    // 두 번째 줄에 "0" 출력
+    cout << "0\n";
+
+    return 0;
+}
