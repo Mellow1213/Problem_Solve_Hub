39b3b4798d4d0a762504e616fa58761b9c5bffee
Yechan Choi <dpcks7282@gmail.com>
2024-03-05 13:03:47 +0900

    [Bronze III] Title: ZOAC 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int H, W, N, M;
+    cin >> H >> W >> N >> M;
+    cout << ((H - 1) / (N + 1) +1) * ((W - 1) / (M + 1) +1);
+    return 0;
+}
