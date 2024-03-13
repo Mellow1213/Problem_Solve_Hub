2615a759d0e1883a436a600ca0a92b6221153af1
Yechan Choi <dpcks7282@gmail.com>
2024-03-13 10:07:40 +0900

    [Silver II] Title: 마인크래프트, Time: 140 ms, Memory: 2876 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+
+int main()
+{
+    int N, M, amount;
+    int UseTime = INT_MAX;
+    int GroundHeight = 0;
+    cin >> N >> M >> amount;
+    
+    int ground[N][M];
+    
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> ground[i][j];
+        }
+    }
+    
+    for(int i=0; i<=256; i++){
+        int needTime = 0;
+        int needBlock = 0;
+        for(int j=0; j<N; j++){
+            for(int k=0; k<M; k++){
+                int blockDelta = 0;
+                blockDelta = i - ground[j][k];
+                
+                needBlock += blockDelta;
+                if(blockDelta >= 0) {
+                    needTime += blockDelta;
+                }
+                else{
+                    needTime -= blockDelta*2;
+                }
+            }
+        }
+        
+        if(needBlock <= amount && needTime <= UseTime) { 
+            UseTime = needTime;
+            GroundHeight = i;
+        }
+    }
+    cout << UseTime << " " << GroundHeight;
+}
