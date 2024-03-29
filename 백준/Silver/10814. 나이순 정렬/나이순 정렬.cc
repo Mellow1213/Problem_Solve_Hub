04c66c45fc9f822bad999eea1290ce62dc02b6a8
Yechan Choi <dpcks7282@gmail.com>
2024-03-29 09:36:27 +0900

    [Silver V] Title: 나이순 정렬, Time: 96 ms, Memory: 5420 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+    
+    string name[N];
+    int age[N];
+    
+    vector<int> haveAge;
+    
+    for(int i=0; i<N; i++){
+        cin >> age[i] >> name[i];
+        
+        if(haveAge.empty()) {
+            haveAge.push_back(age[i]);
+            
+        }
+        else{
+            bool flag = false;
+            int j;
+            for(j=0; j<haveAge.size(); j++){
+                if(age[i] == haveAge[j]){
+                    flag = true;
+                }
+            }
+            if(!flag) 
+                haveAge.push_back(age[i]);
+        }
+        
+    }
+    
+    for(int i=0; i<haveAge.size(); i++){
+        for(int j=i; j<haveAge.size(); j++){
+            if(haveAge[i] > haveAge[j]){
+                int temp = haveAge[i];
+                haveAge[i] = haveAge[j];
+                haveAge[j] = temp;
+            }
+        }
+    }
+    
+    for(int i=0; i<haveAge.size(); i++){
+        for(int j=0; j<N; j++){
+            if(age[j] == haveAge[i]){
+                cout << age[j] << " " << name[j] << '\n';
+            }
+        }
+    }
+}
