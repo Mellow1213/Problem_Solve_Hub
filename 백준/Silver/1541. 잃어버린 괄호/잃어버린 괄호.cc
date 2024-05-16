30e43803a4aa6190ebdcb1c5e43d09305fa0a1d3
Yechan Choi <dpcks7282@gmail.com>
2024-05-16 13:01:40 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,44 +1,38 @@
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <cctype>
 #include <vector>
 
 using namespace std;
 
-int main() {
+void AddTextByNum(int& result, string str, bool addOption){
+    result += addOption ? stoi(str) : -1*stoi(str);
+}
+
+int main()
+{
     string text;
-    cin >> text;
-    
-    vector<string> minus_split;
-    stringstream ss(text);
-    string temp;
-
-    // '-'를 기준으로 문자열을 나누기
-    while (getline(ss, temp, '-')) {
-        minus_split.push_back(temp);
-    }
-
     int result = 0;
-
-    // 첫 번째 부분 문자열은 무조건 더해줌
-    for (int i = 0; i < minus_split.size(); ++i) {
-        int sum = 0;
-        stringstream plus_ss(minus_split[i]);
-        string plus_temp;
-
-        // '+'를 기준으로 부분 문자열을 나누고 더하기
-        while (getline(plus_ss, plus_temp, '+')) {
-            sum += stoi(plus_temp);
+    bool isFirstNum = true;
+    cin >> text;
+    string temp = "";
+    
+    for(int i=0; text[i]!=0; i++){
+        if(text[i] == '-' || text[i] == '+'){
+            if(isFirstNum && text[i] == '+'){
+                AddTextByNum(result, temp, isFirstNum);
+            }
+            else{
+                AddTextByNum(result, temp, isFirstNum);
+                isFirstNum = false;
+            }
+            temp.clear();
         }
-
-        if (i == 0) {
-            result += sum; // 첫 번째 값은 더하기
-        } else {
-            result -= sum; // 나머지 값은 빼기
+        else{
+            temp+=text[i];
         }
     }
-
-    cout << result << endl;
-
-    return 0;
+    
+    AddTextByNum(result, temp, isFirstNum);
+    
+    cout << result;
 }
