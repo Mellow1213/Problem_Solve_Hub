881ae3b84116cb80e43ed4355750e6638f4604d6
Yechan Choi <dpcks7282@gmail.com>
2024-05-17 13:40:43 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    priority_queue<int, vector<int>, greater<int>> q;
+    int testCase;
+    
+    cin >> testCase;
+    
+    for(int t=0; t<testCase; t++){
+        int temp;
+        cin >> temp;
+        
+        if(temp == 0){
+            if(q.empty())
+                cout << 0 << '\n'; // 줄 바꿈 추가
+            else{
+                cout << q.top() << '\n';
+                q.pop();
+            }
+            continue;
+        }
+        
+        q.push(temp);
+    }
+}
