64976b34ce3129636e4779e891559e8f4cea5b4e
Yechan Choi <dpcks7282@gmail.com>
2024-09-20 10:45:44 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -1,32 +1,35 @@
 #include <iostream>
 #include <string>
-#include <stack>
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     string stick;
     cin >> stick;
     
-    stack<int> s;
+    int stack_size = 0;
     int count = 0;
+    int len = stick.length();
     
-    for (int i = 0; i < stick.length(); i++) {
-        if(stick[i]=='('){
-            s.push('(');
+    for (int i = 0; i < len; i++) {
+        if (stick[i] == '(') {
+            stack_size++;
         }
-        else{
-            s.pop();
-            if(stick[i-1] == '('){ // 레이저
-                count += s.size();
+        else {
+            stack_size--;
+            if (stick[i-1] == '(') { // 레이저
+                count += stack_size;
             }
-            else{
+            else {
                 count++;
             }
         }
     }
     
-    cout << count << endl;
+    cout << count << '\n';
     
     return 0;
 }
