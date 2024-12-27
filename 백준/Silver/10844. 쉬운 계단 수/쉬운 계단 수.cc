ee23c0b536b7042206f261aa7eec29d38cb375f6
Yechan Choi <dpcks7282@gmail.com>
2024-12-27 09:39:21 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -3,32 +3,31 @@
 using namespace std;
 
 #define moduler 1000000000
-typedef long long ll;
+
 int main()
 {
     int N;
     cin >> N;
+    if(N==1) {
+        cout << 9;
+        return 0;
+    }
     
-    vector<vector<ll>> dp(N+1, vector<ll>(11)); // dp[길이][끝자리 수];
+    vector<vector<int>> dp(N+1, vector<int>(10)); // dp[길이][끝자리 수];
     
     for(int i=1; i<=9; i++) {
         dp[1][i] = 1;
     }
-    dp[1][0] = 0;
     
     for(int i=2; i<=N; i++) {
-        
-        for(int j=0; j<=9; j++) {
-            if(j==0)
-                dp[i][j] = dp[i-1][j+1] % moduler;
-            else if (j==9)  
-                dp[i][j] = dp[i-1][j-1] % moduler;
-            else            
-                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % moduler;
+        dp[i][0] = dp[i-1][1];
+        dp[i][9] = dp[i-1][8];
+        for(int j=1; j<=8; j++) {
+            dp[i][j] = ( dp[i-1][j-1] + dp[i-1][j+1] ) % moduler;
         }
     }
     
-    ll result = 0;
+    int result = 0;
     for(int i=0; i<=9; i++) {
         result = (result+dp[N][i]) % moduler;
     }
