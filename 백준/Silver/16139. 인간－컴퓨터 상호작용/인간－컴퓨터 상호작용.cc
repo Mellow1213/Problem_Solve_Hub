68ac146d621cfe82f0fb50b2328775fc44fa5db1
Yechan Choi <dpcks7282@gmail.com>
2025-01-13 10:44:16 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 108 ms, Memory: 23964 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,29 +1,67 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+
 using namespace std;
 
-string str;
-int tc;
+static void SetFastIOS() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);  cout.tie(0);    
+}
 
-void Solve() {
-    int counter = 0;
-    int start, end;
-    char ch;
-    cin >> ch >> start >> end;
-    for(int i=start; i<=end; i++) {
-        if(str[i] == ch)
-            counter++;
+class ScanLetter {
+    private: 
+        string str;
+        int size;
+        vector<vector<int>> arr;
+        void InitVector();
+    public:
+        ScanLetter(string str);
+        int ScanChar(char targetChar, int start, int end);
+};
+
+ScanLetter::ScanLetter(string str) : str(str) {
+    size = str.size();
+    arr.assign(26, vector<int>(size, 0)); // a ~ z
+    InitVector();
+}
+
+void ScanLetter::InitVector() {
+    arr[str[0]-'a'][0] = 1;
+    for(int i=1; i<size; i++) {
+        for(int j=0; j<26; j++) {
+            arr[j][i] = arr[j][i-1];
+        }
+        arr[str[i]-'a'][i]++;
     }
-    cout << counter << '\n';
+}
+
+int ScanLetter::ScanChar(char ch, int st, int ed) {
+    const int idx = ch - 'a';
+    int cnt = 0;
+    if(st != 0) {
+        cnt = arr[idx][ed] - arr[idx][st-1];
+    } else if (st == 0){
+        cnt = arr[idx][ed];
+    }
+    
+    return cnt;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    cin >> str >> tc;
-    
-    for(int i=0; i<tc; i++) {
-        Solve();
+    SetFastIOS();
+    string str;
+    int testcase;
+    cin >> str >> testcase;
+    ScanLetter sl(str);
+    for(int i=0; i<testcase; i++) {
+        char ch;
+        int st, ed;
+        cin >> ch >> st >> ed;
+        assert(ch >= 'a' && ch <= 'z');
+        assert(st >= 0 && ed <= str.size());
+        cout << sl.ScanChar(ch, st, ed) << '\n';
     }
+    
 }
