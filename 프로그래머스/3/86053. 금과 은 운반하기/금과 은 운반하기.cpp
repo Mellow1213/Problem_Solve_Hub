47dac5d3fb0f92c5957d4f6c4d30cd6ee7755659
Yechan Choi <dpcks7282@gmail.com>
2025-05-07 12:34:41 +0900

    [level 3] Title: 금과 은 운반하기, Time: 50.29 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.cpp
@@ -0,0 +1,43 @@
+#include <climits>
+#include <vector>
+
+using namespace std;
+
+bool IsPossible(int a, int b, vector<int>& g, vector<int>& s, vector<int>& w, vector<int>& t, long long T) {
+    long long goldSum = 0;
+    long long silverSum = 0;
+    long long mixSum = 0;
+    for(int i=0; i<g.size(); i++) {
+        long long curG = g[i];
+        long long curS = s[i];
+        long long curW = w[i];
+        long long curT = t[i];
+        
+        long long possibleTrip = T / (2*curT);
+        if(T % (2*curT) >= curT)
+            possibleTrip++;
+        
+        long long possibleWeight = possibleTrip * curW;
+        
+        goldSum += min(curG, possibleWeight);
+        silverSum += min(curS, possibleWeight);
+        mixSum += min(curG + curS, possibleWeight);     
+    }
+    return goldSum >= a && silverSum >= b && mixSum >= a+b;
+}
+
+long long solution(int a, int b, vector<int> g, vector<int> s, vector<int> w, vector<int> t) {
+    long long left = 0;
+    long long right = 10e14 * 3;
+    while(left < right) {
+        long long mid = (left + right) / 2;
+        
+        if(IsPossible(a, b, g, s, w, t, mid)) {
+            right = mid;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return left;
+}
