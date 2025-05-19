96b0d8247ae41fc46cc13865ac860f698d5f0b9e
Yechan Choi <dpcks7282@gmail.com>
2025-05-19 10:54:16 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 36 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+int GetOptimizeSortCount(const vector<int>& arr) {
+    assert(arr.size() > 0);
+    
+    if(arr.size() <= 1) { // early-return
+        return 0;
+    }
+    
+    priority_queue<int, vector<int>, greater<int>> pq; // 오름차순. 낮은 것부터 계산해야 하니까
+    
+    for(const auto p : arr) {
+        pq.push(p);
+    }
+    
+    int sum = 0;
+    while(pq.size() > 2) {
+        int temp = 0;
+        for(int i=0; i<2; i++) {
+            temp += pq.top();
+            pq.pop();
+        }
+        sum += (temp);
+        pq.push(temp);
+    }
+    
+    for(int i=0; i<2; i++) {
+        sum += pq.top();
+        pq.pop();
+    }
+    
+    return sum;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> vec(N);
+    for(int i=0; i<N; i++) {
+        cin >> vec[i];
+    }
+    
+    cout << GetOptimizeSortCount(vec);
+}
