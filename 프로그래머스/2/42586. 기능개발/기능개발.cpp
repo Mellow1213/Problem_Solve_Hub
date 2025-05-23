6ef376e3b0737c27e02c9a44bc39d3cbf33b539d
Yechan Choi <dpcks7282@gmail.com>
2025-05-23 10:05:46 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,31 +1,23 @@
 #include <string>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     int size = progresses.size();
-    queue<int> q;
+    int releaseDate = 0;
     for(int i=0; i<size; i++) {
         int leftTime = 100-progresses[i];
         int queueData = (leftTime + speeds[i] -1) / speeds[i]; // ceil 연산
-        q.push(queueData);
-    }
-    
-    while(!q.empty()) {
-        int popCnt = 1; // 우선 front 기능도 배포되었으므로.
-        int curFront = q.front();
-        q.pop();
         
-        while(!q.empty() && q.front() <= curFront) {
-            popCnt++;
-            q.pop();
+        if(answer.empty() || releaseDate < queueData) {
+            answer.push_back(1);
+            releaseDate = queueData;
+        } else {
+            answer.back()++;  
         }
-        
-        answer.push_back(popCnt);
     }
-    return answer;
     
+    return answer;    
 }
