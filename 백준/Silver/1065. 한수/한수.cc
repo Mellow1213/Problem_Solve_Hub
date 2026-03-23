33c86fba3b5777516f6c20effacb36dac66412e0
Yechan Choi <dpcks7282@gmail.com>
2026-03-23 10:58:14 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.cc
+++ 백준/Silver/1065. 한수/한수.cc
@@ -2,31 +2,36 @@
 #include <vector>
 using namespace std;
 
+bool IsHansu(int n) {
+    if(n<100) return true;
+    
+    vector<int> nums;
+    int temp = n;
+    
+    // 입력된 정수의 역순으로 Vector에 저장되나, 연산에는 문제 없음.
+    while(temp > 0) {
+        nums.push_back(temp % 10);
+        temp /= 10;
+    }
+    
+    int d = nums[0] - nums[1];
+    // 공차를 0-1구간으로 설정했으므로 i=2부터 시작.
+    for(int i=2; i<nums.size(); i++) {
+        // 공차 계산 때의 뺄셈 순서를 연산때도 동일하게
+        if(nums[i-1] - nums[i] != d) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     int N;
     int answer = 0;
-    vector<int> temp(4);
     cin >> N;
-    
-    if(N<100) {
-        answer = N;
-    } else if(N == 1000) {
-        answer = 144;
-    } else {
-        answer = 99;
-        for(int i=100; i<=N; i++) {
-            int curNum = i;
-            for(int j=2; j>=0; j--) {
-                temp[j] = curNum%10;
-                curNum /= 10;
-            }
-            if(temp[2]-temp[1] == temp[1]-temp[0]) {
-                answer++;
-            }
-            fill(temp.begin(), temp.end(), 0);
-        }
+    for(int i=1; i<=N; i++) {
+        if(IsHansu(i)) answer++;
     }
-    
     cout << answer;
 }
